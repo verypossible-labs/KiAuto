ccedc1152fd80bf59ec4076630f5d46d6602b178
Salvador E. Tropea <salvador@inti.gob.ar>
2020-03-04 12:59:57 -0300

    Added experimental version of wait_for_file_created_by_process from upstream

--- util/file_util.py
+++ util/file_util.py
@@ -21,7 +21,7 @@ def mkdir_p(path):
 def wait_for_file_created_by_process(pid, file, timeout=5):
     process = psutil.Process(pid)
 
-    DELAY = 0.05
+    DELAY = 0.5
     for i in range(int(timeout/DELAY)):
         if os.path.isfile(file):
             if file in process.open_files():
@@ -35,3 +35,26 @@ def wait_for_file_created_by_process(pid, file, timeout=5):
         time.sleep(DELAY)
 
     raise RuntimeError('Timed out waiting for creation of %s' % file)
+
+# def wait_for_file_created_by_process(pid, file, timeout=5):
+#     process = psutil.Process(pid)
+# 
+#     DELAY = 0.01
+#     for i in range(int(timeout/DELAY)):
+#         open_files = process.open_files()
+#         logger.debug(open_files)
+#         if os.path.isfile(file):
+#             file_open = False
+#             for open_file in open_files:
+#                 if open_file.path == file:
+#                     file_open = True
+#             
+#             if file_open:
+#                 logger.debug('Waiting for process to close file')
+#             else:
+#                 return
+#         else:
+#             logger.debug('Waiting for process to create file')
+#         time.sleep(DELAY)
+# 
+#     raise RuntimeError('Timed out waiting for creation of %s' % file)
