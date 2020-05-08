4fe15a37592b5becc159775331fe9b05d5c696f2
Salvador E. Tropea <salvador@inti.gob.ar>
2020-05-08 10:51:37 -0300

    Moved return codes and default wait value to misc.py. Also removed a try that won't fail because an if and the args parser protects the code.

--- kicad_auto/misc.py
+++ kicad_auto/misc.py
@@ -2,4 +2,15 @@
 REC_W = 1366
 REC_H = 768
 
+# Return error codes
+# Positive values are ERC/DRC errors
+NO_SCHEMATIC = 1
+EESCHEMA_CFG_PRESENT = 2
+KICAD_CFG_PRESENT = 3
+NO_PCB = 4
+PCBNEW_CFG_PRESENT = 5
+# Wait 25 s to pcbnew/eeschema window to be present
+WAIT_START = 25
+
+
 __version__ = '1.1.6'
